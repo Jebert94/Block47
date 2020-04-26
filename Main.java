f532276fd9acbfb109f1a6caa62611bd58045027
Jebert94 <Trebe94@gmail.com>
2020-04-26 19:07:05 -0400

    Main method completed to include all of Introduction

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.print("\033[H\033[2J"+"\n");
         Intro intro = new Intro();  
-        Intro.description();
-        Intro.description();
+        Intro.introduction();
         Player player = Player.getInstance();
         intro.createPlayerName(player);
+        Intro.background();
         intro.createPlayerStats(player);
     }
 }

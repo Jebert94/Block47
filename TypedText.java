85bd2050464c6300383a02673be6ebf5acf95053
Jebert94 <Trebe94@gmail.com>
2020-04-26 17:59:06 -0400

    Adjusted all TypedText methods to create their own new line once finished typing. Also created "optional parameters" for the fast, normal and slow speeds to adjust the wait times until going to the next line on the fly.

--- TypedText.java
+++ TypedText.java
@@ -1,8 +1,18 @@
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class TypedText {
+    private static int defaultWaitTime1 = 1;
+    private static int defaultWaitTime2 = 2;
 
-    public static void typedFast(String line) {
+
+/* 
+* Overrided methods typedFast(), typedNormal(), and typedSlow() to act like they have optional peramiters.
+* The optional perameter being a int called waitTime
+* It's pourpose is to determine how long to wait until printing a new line
+*/
+
+    public static void typedFast(String line, int waitTime) throws InterruptedException {
         for(int i =0; i<line.length();i++){
             System.out.print(line.charAt(i));
             try {
@@ -11,9 +21,20 @@ public class TypedText {
                 e.printStackTrace();
             }
         }
+        TimeUnit.SECONDS.sleep(waitTime);
+        System.out.println();
     }
 
-    public static void typedNormal(String line) {
+    public static void typedFast(String line) throws InterruptedException {
+        typedFast(line, defaultWaitTime1);
+        
+    }
+
+
+
+
+
+    public static void typedNormal(String line, int waitTime) throws InterruptedException {
         for(int i =0; i<line.length();i++){
             System.out.print(line.charAt(i));
             try {
@@ -22,9 +43,19 @@ public class TypedText {
                 e.printStackTrace();
             }
         }
+        TimeUnit.SECONDS.sleep(waitTime);
+        System.out.println();
+    }
+    public static void typedNormal(String line) throws InterruptedException {
+        typedNormal(line, defaultWaitTime2);
     }
 
-    public static void typedSlow(String line) {
+
+
+
+
+
+    public static void typedSlow(String line, int waitTime) throws InterruptedException {
         for(int i =0; i<line.length();i++){
             System.out.print(line.charAt(i));
             try {
@@ -33,9 +64,19 @@ public class TypedText {
                 e.printStackTrace();
             }
         }
+        TimeUnit.SECONDS.sleep(waitTime);
+        System.out.println();
     }
 
-    public static void typedRandom(String line) {
+    public static void typedSlow(String line) throws InterruptedException {
+        typedSlow(line, defaultWaitTime2);
+    }
+
+
+
+
+
+    public static void typedRandom(String line) throws InterruptedException {
         Random rand = new Random(); 
         for(int i =0; i<line.length();i++){
             System.out.print(line.charAt(i));
@@ -46,9 +87,14 @@ public class TypedText {
                 e.printStackTrace();
             }
         }
+        TimeUnit.SECONDS.sleep(defaultWaitTime1);
+        System.out.println();
     }
 
-    public static void typedSarcasticly(String line) {
+
+
+
+    public static void typedSarcasticly(String line) throws InterruptedException {
         for(int i =0; i<line.length();i++){
             if(i % 2 == 0){
                 char evenLetter = line.charAt(i);
@@ -67,5 +113,7 @@ public class TypedText {
                 e.printStackTrace();
             }
         }
+        TimeUnit.SECONDS.sleep(defaultWaitTime1);
+        System.out.println();
     }
 }

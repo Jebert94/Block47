60fe3cd36fc37f2ee8b9bf6b4a97af6159b98a45
Jake Richards <46974224+jdr121996@users.noreply.github.com>
2020-05-03 21:45:30 -0400

    Adding new item object files and item dependency

---
+++ ItemInformation.java
@@ -0,0 +1,7 @@
+package idontcare;
+
+public abstract class ItemInformation {
+	 public abstract String itemName();
+	 public abstract int positiveAttackModifier();
+	 // public abstract int negativeAttackModifier(); If we decide to add negative stat effects
+}

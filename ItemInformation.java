5f98fd71f831e6130f361ec4bdbd0d3f613c0b8f
Jebert94 <Trebe94@gmail.com>
2020-05-07 23:48:46 -0400

    Removed these classes as they can now be create on the fly with the items class constructor

--- ItemInformation.java
+++
@@ -1,7 +0,0 @@
-package idontcare;
-
-public abstract class ItemInformation {
-	 public abstract String itemName();
-	 public abstract int positiveAttackModifier();
-	 // public abstract int negativeAttackModifier(); If we decide to add negative stat effects
-}

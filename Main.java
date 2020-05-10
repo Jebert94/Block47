469359e78b089a670162c74d8e5e618de05afd79
Josh <Trebe94@gmail.com>
2020-05-10 07:54:20 -0400

    Added a simpler and more reliable way to emulate clearing the screen

--- Main.java
+++ Main.java
@@ -2,14 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner input = new Scanner(System.in);
+    
+    public static void clearScreen(){
+        for(int i = 0; i<50; i++){
+            System.out.println();
+        }
+    }
 
 	public static void main(String[] args) throws InterruptedException {
-        System.out.print("\033[H\033[2J"+"\n");
         Intro intro = new Intro();  
         //Intro.introduction();
+        //intro.title();
         Player player = Player.getInstance();
+        Intro.background();
         intro.createPlayerName(player);
-        //Intro.background();
         intro.createPlayerStats(player);
         Gameplay.mainGameplay();
 

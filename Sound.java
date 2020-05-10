53cba36eaf71f5d9d41ffe81e3cb2cb31bc82986
Josh <Trebe94@gmail.com>
2020-05-10 17:17:42 -0400

    Modified sound class so the array of key clicks are not added in constructor

--- Sound.java
+++ Sound.java
@@ -6,9 +6,8 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class Sound {
-    private ArrayList<String> typingFiles = new ArrayList<String>();
-
-    public  Sound(){
+    private static ArrayList<String> typingFiles = new ArrayList<String>();
+    static{
         typingFiles.add("/SoundEffects/Old01.wav");
         typingFiles.add("/SoundEffects/Old02.wav");
         typingFiles.add("/SoundEffects/Old03.wav");
@@ -16,20 +15,27 @@ public class Sound {
         typingFiles.add("/SoundEffects/Old05.wav");
         typingFiles.add("/SoundEffects/Old05.wav");
     }
+    private Clip clip;
 
-    public void sound(String file) {
+    public Sound(String file) {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(this.getClass().getResource(file));
-            Clip clip = AudioSystem.getClip();
+            this.clip = AudioSystem.getClip();
             clip.open(audioInputStream);
             clip.start();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
+    
+
+    public Clip getClip(){
+        return clip;
+    }
 
 
-    public void typingSound() {
+
+    public Sound() {
         Random rand = new Random();
         try {
             String file = typingFiles.get(rand.nextInt(typingFiles.size()));
